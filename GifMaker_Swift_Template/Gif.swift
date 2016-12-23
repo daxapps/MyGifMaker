70d7a6c49ec6c92e91e47e59d59f6ba2900cdfda
daxapps <daxapps777@gmail.com>
2016-12-23 17:33:11 -0600

    conform to NSCoding Protocol

--- GifMaker_Swift_Template/Gif.swift
+++ GifMaker_Swift_Template/Gif.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class Gif: NSObject {
+class Gif: NSObject, NSCoding {
     
     let url: NSURL?
     let videoURL: NSURL?
@@ -26,7 +26,25 @@ class Gif: NSObject {
         self.gifData = nil
     }
     
+    required init?(coder decoder: NSCoder) {
+        self.url = decoder.decodeObject(forKey: "url") as? NSURL
+        self.videoURL = decoder.decodeObject(forKey: "videoURL") as! NSURL
+        self.caption = decoder.decodeObject(forKey: "caption") as? String
+        self.gifImage = decoder.decodeObject(forKey: "gifImage") as? UIImage
+        self.gifData = decoder.decodeObject(forKey: "gifData") as? NSData
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.url, forKey: "url")
+        coder.encode(self.videoURL, forKey: "videoURL")
+        coder.encode(self.caption, forKey: "caption")
+        coder.encode(self.gifImage, forKey: "gifImage")
+        coder.encode(self.gifData, forKey:"gifData")
+    }
 //    init(name: String) {
 //        self.gifImage = UIImage.gif(name: name)
 //    }
 }
+
+
+
